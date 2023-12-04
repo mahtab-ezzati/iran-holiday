0d4b795fe9d6cf3ad4002f0d781c8a8b84049c75
mahtab ezzati <mezati79@gmail.com>
2023-12-04 22:12:28 +0330

    sad and happy page
    update database
    add 2 page for main page

--- website/isholiday.py
+++ website/isholiday.py
@@ -2,13 +2,15 @@ import sqlite3
 
 
 def find_nearest_holiday(date):
-    conn = sqlite3.connect('persian holiday 04.db')
+    conn = sqlite3.connect('persian holiday.sqlite3')
     cur = conn.cursor()
     cur.execute(f'select * from holiday where date = \'{date}\'')
-    index = cur.fetchall()[0][0]
+    input_index = cur.fetchall()[0][0]
 
-    while cur.execute(f'select * from holiday where id = {index}').fetchone()[7] != 'Y':
-        index += 1
-
-    nearest_holiday =  cur.execute(f'select * from holiday where id = {index}').fetchone()
-    return f'{nearest_holiday[1]} {nearest_holiday[6]}'
+    nearest_index = input_index
+    while cur.execute(f'select * from holiday where id = {nearest_index}').fetchone()[8] != 'Y':
+        nearest_index += 1
+    
+    nearest_holiday =  cur.execute(f'select * from holiday where id = {nearest_index}').fetchone()
+    print(nearest_holiday)
+    return {'distance': nearest_index-input_index, 'date': nearest_holiday[1], 'info':nearest_holiday[-1]}
--- website/views.py
+++ website/views.py
@@ -7,7 +7,15 @@ import jdatetime
 # Create your views here.
 def main_page(request):
     today = str(jdatetime.datetime.now())[:10]
+    # today = '1402-09-26'
     status = isholiday.find_nearest_holiday(today)
-        
-    return render(request, 'happy.html', context={'status' : status})
+
+    if status['distance'] == 0:
+        status.update({'happy': 'امروز'})
+        return render(request, 'happy.html', context = status)
+    elif status['distance'] == 1:
+        status.update({'happy': 'فردا'})
+        return render(request, 'happy.html', context = status)
+    else:
+        return render(request, 'sad.html', context = status)
 
